d569064e55b31456c66fba92f96326a80a295618
Lukasz Stelmach <lukasz.stelmach@iem.pw.edu.pl>
2008-12-11 15:21:25 +0100

    [project @ lukasz.stelmach@iem.pw.edu.pl-20081211142125-r0rw9kr97hybp1no] Ramka "Blip": html, css, JS.

--- blip.js
+++ blip.js
@@ -1,3 +1,20 @@
 /*
  * $Id$
  */
+
+function showBlip(b) {
+	var linkre=/(http:\/\/rdir.pl.[A-Za-z0-9]+)/g;
+	var i;
+	for (i=0; i < b.length; i++) {
+		/*
+		 * b[i].body - tekst wpisu
+		 * b[i].id - identyfikator do zastosowania w linkach
+		 */
+		var body=b[i].body;
+		body=body.replace(linkre, '<a href="$1">[link]</a>');
+		$('blipentries').insert("<li>" + body + "</li>");
+	}
+	$('blip').show();
+}
+
+showBlip(Blip);
